b6f3c8cd6bbd2f157e16837997719a06aa6d4fd9
Sid Mason-Malik <49129863+smasonmalik@users.noreply.github.com>
2019-11-12 16:43:53 +0000

    login form - attempt

---
+++ src/components/login.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class Login extends Component {
+  constructor(props) {
+    super(props)
+      this.state = {
+        email: '',
+        password: ''
+      }
+  }
+  render() {
+    return (
+      <div>
+         <h4>Log in</h4>
+         <form onSubmit={e => {e.preventDefault();}}>
+           <div className='form-input'>
+             <input label="Email"
+                    class="email"
+                   type="email"
+                   name="email" />
+             <input label="Password"
+                    class="password"
+                   type="password"
+                     name="password" />
+           </div>
+           <button handleClick={this.handleLogin}
+                   name='login'
+                   class='login'
+                   label='Log In'/>
+         </form>
+         <a href='/sign_up'>
+           <button name='signup'
+                   class='secondary'
+                   label='Sign Up'/>
+                   </a>
+      </div>
+    );
+  }
+}
+export default Login;
