974c6200a60e6bf74d3f06fb8326875bfcb407a6
zraso <zaira.rasool@hotmail.co.uk>
2019-11-14 16:23:42 +0000

    Login logout toggle on navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,40 +1,48 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import Logout from './logout';
-import {Link} from 'react-router-dom'
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Logout from "./logout";
+import { Link } from "react-router-dom";
 
 class Navbar extends Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
 
   sessionButton() {
     if (this.props.authToken) {
-      return <Logout updateAuthToken={this.props.updateAuthToken}/>
+      return <Logout updateAuthToken={this.props.updateAuthToken} />;
     } else {
-      return null 
+      return (
+        <Link to="/log_in">
+          <a className="homepage-link" href="javascript:void(0)">
+            Login
+          </a>
+        </Link>
+      );
     }
   }
   render() {
     return (
       <header>
         <nav className="navbar-container">
-          <Link to='/'>
-            <a className="homepage-link" href="javascript:void(0)">Winkleface</a>
+          <Link to="/">
+            <a className="homepage-link" href="javascript:void(0)">
+              Winkleface
+            </a>
           </Link>
-          <ul className="logout-button">
-            {this.sessionButton()}
-          </ul>
+          <ul className="logout-button">{this.sessionButton()}</ul>
 
           <ul>
-            <Link to='/posts'>
-              <a className="homepage-link" href="javascript:void(0)">Home</a>
+            <Link to="/posts">
+              <a className="homepage-link" href="javascript:void(0)">
+                Home
+              </a>
             </Link>
           </ul>
         </nav>
       </header>
-    )
+    );
   }
 }
 
-export default Navbar
+export default Navbar;
