e0ccf5eda3a0220d41f738d89288b75331a18666
Nat Elliott-Green <natelliottgreen34@gmail.com>
2019-11-14 11:03:12 +0000

    Created navbar and logout components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/login';
+import Navbar from './components/navbar';
 
 
 class App extends Component {
@@ -14,20 +15,11 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Hello World!
-          </a>
+          <Navbar/>
         </header>
+
         <Login/>
+        
       </div>
     );
   }
