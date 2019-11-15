b759f55779c87f079ff009895a7fa9ed0088d35c
kevindhanna <kevindhanna@gmail.com>
2019-11-15 14:29:10 +0000

    add post styling

--- src/components/singleComment.jsx
+++ src/components/singleComment.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Likes from './likes'
+import './singleComment.css'
 
 class Comment extends Component {
 
@@ -7,19 +8,25 @@ class Comment extends Component {
 
   render() {
     return (
-      <div className='comment'>
-        <div className='comment-message'>
-          message:{this.props.message}
-        </div>
-        <div className='comment-username'>
-          <small> Username:{this.props.username} </small>
-        </div>
-        <div className='comment-created_at'>
-          <small> Created at: {this.props.created_at} </small>
-        </div>
-        <div className='comment-like'>
-        <Likes get_likes={this.props.get_likes}/>
-        </div>
+        <div className='comment'>
+          <div className='comment-username'>
+            <small> {this.props.username} says: </small>
+          </div>
+          <div className='comment-message'>
+            {this.props.message}
+          </div>
+          <div className='rows'>
+            <div className='col'>
+            <div className='comment-created_at'>
+            <small> {new Date(Date.parse(this.props.created_at)).toDateString()} </small>
+            </div>
+            </div>
+            <div className='col'>
+            <div className='comment-like'>
+              <Likes get_likes={this.props.get_likes}/>
+            </div>
+            </div>
+          </div>
       </div>
     )
   }
